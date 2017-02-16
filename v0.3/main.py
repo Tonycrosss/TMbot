c19419d3293509c0b81d683a636f799676e071e6
TonyCross <starovoitov.anton@gmail.com>
2017-02-17 04:06:38 +0500

    testing bot1

--- v0.3/main.py
+++ v0.3/main.py
@@ -1,6 +1,16 @@
 from tmbot import tmbot
 from tmbot import config
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
+import subprocess, sys
+from selenium import common
+
+
+
+
+
 
 tm = tmbot.TwitchMaster(config.user_login, config.user_password)
 
-tm.login()
+tm.bot1()
