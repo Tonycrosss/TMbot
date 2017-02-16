4516c17ed97a4cfd60d34223651c51be09cd8e71
TonyCross <starovoitov.anton@gmail.com>
2017-02-16 16:38:09 +0500

    authentication

--- v0.3/tmbot/tmbot.py
+++ v0.3/tmbot/tmbot.py
@@ -4,6 +4,7 @@ import time
 import subprocess, sys
 from selenium import common
 
+
 class TwitchMaster:
     def __init__(self, userlogin, userpassword):
         self.userlogin = userlogin
@@ -13,7 +14,25 @@ class TwitchMaster:
         chrome_options = webdriver.ChromeOptions()
         chrome_options.add_argument("--mute-audio")
         driver = webdriver.Chrome(chrome_options=chrome_options)
-
         driver.get('http://twitchmaster.ru/')  # Коннектимся
         time.sleep(4)
-
+        # xpath для кнопки Вход
+        login_xbutton = "/html/body/div[@class='wrapper']/div[@class='container'][1]/div[@class='header']/div[@class='links f-right']/span[@class='menu-content']/a[@class='ajax-popup']"
+        # Нажимаем на кнопку
+        driver.find_element_by_xpath(login_xbutton).click()
+        time.sleep(1)
+        # Xpath Для поля - Имя пользователя
+        login_xfield = "/html/body/div[@id='popup-window']/div[@class='inner']/form/input[1]"
+        # Вбиваем логин
+        driver.find_element_by_xpath(login_xfield).send_keys(self.userlogin)
+        time.sleep(1)
+        # Xpath Для поля - Пароль
+        passwd_xfield = "/html/body/div[@id='popup-window']/div[@class='inner']/form/input[2]"
+        # Вбиваем пароль
+        driver.find_element_by_xpath(passwd_xfield).send_keys(self.userpassword)
+        time.sleep(1)
+        # Xpath Для кнопки Войти
+        enter_xbutton = "/html/body/div[@id='popup-window']/div[@class='inner']/form/div[@class='center']/div[@class='cool-button form-submitter']"
+        # Жмакаем по кнопке
+        driver.find_element_by_xpath(enter_xbutton).click()
+        time.sleep(5)
