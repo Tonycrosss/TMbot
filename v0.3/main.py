787d7f0e153bc65e7002d50b789b672cc1dd9111
TonyCross <starovoitov.anton@gmail.com>
2017-02-17 06:34:57 +0500

    threads realized

--- v0.3/main.py
+++ v0.3/main.py
@@ -1,7 +1,27 @@
 from tmbot import tmbot
 from tmbot import config
+import time
+import threading
 
 
-tm = tmbot.TwitchMaster(config.user_login, config.user_password)
+if __name__ == '__main__':
 
-tm.bot1()
+    tm = tmbot.TwitchMaster(config.user_login, config.user_password)
+    b1 = tm.bot1
+    b2 = tm.bot2
+    b3 = tm.bot3
+
+    th1 = threading.Thread(target=b1)
+    th2 = threading.Thread(target=b2)
+    th3 = threading.Thread(target=b3)
+    th1.setDaemon(True)
+    th2.setDaemon(True)
+    th3.setDaemon(True)
+    th1.start()
+    time.sleep(35)
+    th2.start()
+    time.sleep(35)
+    th3.start()
+    while True: pass
+
+
